0fb58527eaf6f1c18b9e6447775a8699c702cb65
Mydah-Nasir <mydahnasir311@gmail.com>
2024-04-25 12:59:40 +0500

    removing files

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
